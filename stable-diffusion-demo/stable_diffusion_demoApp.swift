579eb93430faa53150f4b047ba99cb397a5ac43d
Lambo77 <lambozhuang@icloud.com>
2022-12-04 20:40:15 +0800

    Initial Commit

---
+++ stable-diffusion-demo/stable_diffusion_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  stable_diffusion_demoApp.swift
+//  stable-diffusion-demo
+//
+//  Created by Lambo77 on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct stable_diffusion_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
